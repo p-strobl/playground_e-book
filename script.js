fad61dcc9f69678a31ff35cb136b4842c4024744
P. Strobl <p.strobl.hh@gmail.com>
2018-06-23 19:45:12 +0200

    Kranken Test

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 "use strict";
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+{
 
-/**
- * Open Index menubar
- * @returns {undefined}
- */
-const openIndexMenu = () => {
-  $('.open_index_menu_wrap').classList.add('hide_menu');
-  $('.index_side_menu_wrap').classList.add('status_open');
-  $('.main_content').classList.add('shrink_padding');
-};
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
 
-/**
- * Close Index menubar
- * @returns {undefined}
- */
-const closeIndexMenu = () => {
-    $('.open_index_menu_wrap').classList.remove('hide_menu');
-    $('.index_side_menu_wrap').classList.remove('status_open');
-    $('.main_content').classList.remove('shrink_padding');
-};
+  /**
+   * Open Index menubar
+   * @returns {undefined}
+   */
+  const openIndexMenu = () => {
+    $('.open_index_menu_wrap').addEventListener('click', () => {
+      $('.open_index_menu_wrap').classList.add('hide_menu');
+      $('.index_side_menu_wrap').classList.add('status_open');
+      $('.main_content').classList.add('shrink_padding');
+    });
+  };
 
-/**
- * Create List items for index menubar
- * @returns {mixin} HTML h2 list
- */
-const fillIndexManubar = () => {
-  const findAllH2Tags = Array.from($$('h2'));
+  /**
+   * Close Index menubar
+   * @returns {undefined}
+   */
+  const closeIndexMenu = () => {
+    $('.close_index_menu_wrap').addEventListener('click', () => {
+      $('.open_index_menu_wrap').classList.remove('hide_menu');
+      $('.index_side_menu_wrap').classList.remove('status_open');
+      $('.main_content').classList.remove('shrink_padding');
+    });
+  };
 
-};
+  /**
+   * Create List items for index menubar
+   * @returns {mixin} HTML h2 list
+   */
+  const fillIndexManubar = () => {
+    const findAllH2Tags = Array.from($$('h2'));
 
-window.onload = () => {
+  };
+
   openIndexMenu();
   closeIndexMenu();
   fillIndexManubar();
-};
+
+}
+
