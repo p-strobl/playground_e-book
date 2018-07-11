39eb681f244e318476d8b8d45341d402817c3a1b
P. Strobl <p.strobl.hh@gmail.com>
2018-07-11 13:29:54 +0200

    tidy up highlight effect fn

--- src/js/effect_highlight-navbar.js
+++ src/js/effect_highlight-navbar.js
@@ -14,22 +14,43 @@ const removeClassAttrFromEach = (nodes, classSelector) =>
   nodes.forEach(node => node.classList.remove(classSelector));
 
 /**
+ * Determine viewport y axis top position
+ * @returns {number}
+ * */
+const windowScrollYTopPosition = () => Math.round(window.scrollY);
+
+/**
+ * Determine viewport y axis center position
+ * @returns {number}
+ * */
+const windowScrollYCenterPosition = () => Math.round(window.scrollY + (window.innerHeight / 2));
+
+/**
+ * Find article node wich is currently in the center of viewport
+ * @returns {HTMLElement}
+ * */
+const matchedArticleNode = articleNodes => {
+  return articleNodes.find(article =>
+    windowScrollYCenterPosition() >= article.offsetTop &&
+    windowScrollYCenterPosition() <= article.offsetTop + article.offsetHeight);
+};
+
+/**
+ * Find index_li node matches main_article node
+ * @returns {HTMLElement}
+ * */
+const matchedLiAnchorNode = (indexMenuAnchorNodes, articleNodes) =>
+  indexMenuAnchorNodes.find(aNode => aNode.name === matchedArticleNode(articleNodes).id);
+
+/**
  * Based on article node window y position, adds effect to matched index menu li node
  * @returns {onchange}
  * */
 export const highlightIndexLiNode = (indexMenuAnchorNodes, articleNodes, indexMenuHeader) => {
   removeClassAttrFromEach(indexMenuAnchorNodes, 'highlight_by_scroll');
-  const windowScrollYTopPosition = Math.round(window.scrollY);
-  const windowScrollYCenterPosition = Math.round(windowScrollYTopPosition + (window.innerHeight / 2));
-  const matchedArticleNode = articleNodes.find(article =>
-    windowScrollYCenterPosition >= article.offsetTop &&
-    windowScrollYCenterPosition <= article.offsetTop + article.offsetHeight);
-  if (matchedArticleNode !== undefined) {
-    const matchedLiAnchorNode = indexMenuAnchorNodes.find(aNode => aNode.name === matchedArticleNode.id);
-    if (matchedArticleNode) {
-      addClassAttrTo(matchedLiAnchorNode, 'highlight_by_scroll');
-      matchedLiAnchorNode.scrollIntoView();
-    }
-    if (windowScrollYTopPosition <= indexMenuAnchorNodes[0].offsetTop) indexMenuHeader.scrollIntoView();
+  if (matchedArticleNode(articleNodes) !== undefined) {
+      addClassAttrTo(matchedLiAnchorNode(indexMenuAnchorNodes, articleNodes), 'highlight_by_scroll');
+      matchedLiAnchorNode(indexMenuAnchorNodes, articleNodes).scrollIntoView();
+    if (windowScrollYTopPosition() <= indexMenuAnchorNodes[0].offsetTop) indexMenuHeader.scrollIntoView();
   }
 };
