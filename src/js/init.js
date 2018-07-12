1158c0c8a2e6998a7ad133ca9730c61de4da9950
P. Strobl <p.strobl.hh@gmail.com>
2018-07-12 23:30:29 +0200

    folder restruction

--- src/js/init.js
+++ src/js/init.js
@@ -8,8 +8,8 @@ import {
 } from "./helper/helper_dom.js";
 import {addIdToArticleNodes, addPageNrToArticleFooterSpans} from "./actions/edit_articleNode.js";
 import {appendLisToIndexNavbar} from "./actions/create_navbar.js";
-import {initOnClick} from "./event/event_onClick.js";
-import {initOnScroll} from "./event/event_onScroll.js";
+import {initOnClick} from "./events/event_onClick.js";
+import {initOnScroll} from "./events/event_onScroll.js";
 
 /**
  * init DOM completion functions
@@ -22,7 +22,7 @@ const initDomCompletion = () => {
 };
 
 /**
- * init DOM event listener
+ * init DOM events listener
  * @returns {undefined}
  * */
 const initDocumentEvents = () => {
