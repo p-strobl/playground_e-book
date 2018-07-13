ad8c8b5ba1a6e53abe160f14bf407243dd11725e
P. Strobl <p.strobl.hh@gmail.com>
2018-07-13 03:05:33 +0200

    renamed file structure

--- src/js/actions/toggle.cssClass.js
+++ src/js/actions/toggle.cssClass.js
@@ -2,5 +2,5 @@
  * Toggle "open_index_menu_button", "main_content" and "index_menu_wrap" class
  * @returns {event}
  * */
-export const toggleIndexMenu = ([...toggleElements], cssClass) =>
+export const toggleCssClassFrom = ([...toggleElements], cssClass) =>
   toggleElements.forEach(element => element.classList.toggle(cssClass));
--- src/js/menu.tableOfContents/events/onScroll.highlightNode.js
+++ src/js/menu.tableOfContents/events/onScroll.highlightNode.js
@@ -1,13 +1,11 @@
-import {getIndexMenuAnchorNodes, getMainArticleNodes, getIndexMenuHeader} from "../helper/helper_dom.js";
-import {highlightListNode} from "../menu.tableOfContents/highlight.listNode.js";
+import {getIndexMenuAnchorNodes, getMainArticleNodes, getIndexMenuHeader} from "../../helper/get.domNodes.js";
+import {highlightMenuNodeByArticleOnSight} from "../highlight.listNode.js";
 
 /**
  * Event listining for scroll
  * @returns {event}
  * */
 export const initOnScroll = () => {
-  //
   document.addEventListener("scroll", () =>
-    highlightListNode(getIndexMenuAnchorNodes(), getMainArticleNodes(), getIndexMenuHeader()));
-//  hightlightMenuNodeByArticle
+    highlightMenuNodeByArticleOnSight(getIndexMenuAnchorNodes(), getMainArticleNodes(), getIndexMenuHeader()));
 };
